cb8fdba5afbec44626fd56ebd43b5b40e22023b9
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2022-05-06 00:59:03 +0430

    rel="noreferrer" added to link

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,11 @@ import React from "react";
 function Footer() {
   return (
     <p className="Footer">
-      <a href="https://github.com/vida-khalili/weather-react" target={"_blank"}>
+      <a
+        href="https://github.com/vida-khalili/weather-react"
+        target={"_blank"}
+        rel={"noreferrer"}
+      >
         Open-source
       </a>{" "}
       code by Vida Khalili
