a652fe939810b3000d890b2e842a02c625198dae
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2022-05-06 00:39:42 +0430

    react-app created for the Weather app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./style.css";
+import SearchEngine from "./SearchEngine";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <SearchEngine />
+    </div>
+  );
+}
+
+export default App;
