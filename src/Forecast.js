18a7eb7fecccf58c9bf32e35012d0f805c934942
v.khalili <vida.khalili92@gmail.com>
2022-05-18 12:11:25 +0430

    Code cleaned (classNames and ...)

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,49 +1,24 @@
 import React from "react";
-import Sunny from "./img/icons/icon_01d.svg"
-import "./Forecast.css"
+import Sunny from "./img/icons/icon_01d.svg";
+import "./Forecast.css";
 
 function Forecast() {
-
-    return (
-        <div className="Forecast">
-            <div className="ForecastCard">
-                <ul>
-                    <li className="ForecastDay">Sat</li>
-                    <li className="ForecastIcon"><img src={Sunny} alt={"icon"}/></li>
-                    <li className="ForecastTemperature"><span className="minTemp">25°</span><span className="maxTemp">27°</span></li>
-                </ul>
-            </div>
-            <div className="ForecastCard">
-                <ul>
-                    <li className="ForecastDay">Sat</li>
-                    <li className="ForecastIcon"><img src={Sunny} alt={"icon"}/></li>
-                    <li className="ForecastTemperature"><span className="minTemp">25°</span><span className="maxTemp">27°</span></li>
-                </ul>
-            </div>
-            <div className="ForecastCard">
-                <ul>
-                    <li className="ForecastDay">Sat</li>
-                    <li className="ForecastIcon"><img src={Sunny} alt={"icon"}/></li>
-                    <li className="ForecastTemperature"><span className="minTemp">25°</span><span className="maxTemp">27°</span></li>
-                </ul>
-            </div>
-            <div className="ForecastCard">
-                <ul>
-                    <li className="ForecastDay">Sat</li>
-                    <li className="ForecastIcon"><img src={Sunny} alt={"icon"}/></li>
-                    <li className="ForecastTemperature"><span className="minTemp">25°</span><span className="maxTemp">27°</span></li>
-                </ul>
-            </div>
-            <div className="ForecastCard">
-                <ul>
-                    <li className="ForecastDay">Sat</li>
-                    <li className="ForecastIcon"><img src={Sunny} alt={"icon"}/></li>
-                    <li className="ForecastTemperature"><span className="minTemp">25°</span><span className="maxTemp">27°</span></li>
-                </ul>
-            </div>
+  return (
+    <div className="Forecast">
+      <div className="forecast-card">
+        <ul>
+          <li className="forecast-day">Sat</li>
+          <li className="forecast-icon">
+            <img src={Sunny} alt={"icon"} />
+          </li>
+          <li className="forecast-temperature">
+            <span className="min-temp">25°</span>
+            <span className="max-temp">27°</span>
+          </li>
+        </ul>
+      </div>
         </div>
-    )
-
+  );
 }
 
 export default Forecast;
