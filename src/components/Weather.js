bb059fc51be280f7c383e1f986143fb44fc2e4d1
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2022-06-02 00:01:07 +0430

    Loading bars added
    Loading bars added and background changed

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,6 +3,7 @@ import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "../styles/Weather.css";
 import Forecast from "./Forecast";
+import LoadingBars from "../img/loadingBars.svg";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -41,30 +42,34 @@ export default function Weather(props) {
     return (
       <div className="Weather">
         <div className=" current-weather">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9 ">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control search-input"
-                onChange={handleCityChange}
-              />
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-9 ">
+                <input
+                  type="search"
+                  placeholder="Enter a city.."
+                  className="form-control search-input"
+                  onChange={handleCityChange}
+                />
+              </div>
+              <div className="col-3 p-0">
+                <button className="btn btn-primary w-100">
+                  <i className="fa-solid fa-magnifying-glass" />
+                </button>
+              </div>
             </div>
-            <div className="col-3 p-0">
-              <button className="btn btn-primary w-100">
-                <i className="fa-solid fa-magnifying-glass"/>
-              </button>
-            </div>
-          </div>
-        </form>
-        <WeatherInfo data={weatherData} />
+          </form>
+          <WeatherInfo data={weatherData} />
         </div>
         <Forecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
     search();
-    return "Loading...";
+    return (
+      <div className="loading-bars">
+        <img src={LoadingBars} alt="loading" className="img-fluid" />
+      </div>
+    );
   }
 }
